15b075327991f4412fbc227e78c649444e7e5e9d
Learn Programming <pcktuts@gmail.com>
2021-09-23 19:33:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace MyFirstProject
+{
+    class Program
+    {
+        delegate int ChangeNumber(int n);
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Add10(20));
+            Console.WriteLine(Multiply10(100));
+            ChangeNumber del1 = new ChangeNumber(Add10);
+            Console.WriteLine( del1.Invoke(20));
+
+            ChangeNumber del2 = new ChangeNumber(Multiply10);
+            Console.WriteLine(del2.Invoke(100));
+            Console.WriteLine(del2(100));
+
+
+        }
+        static int Add10(int n)
+        {
+            return n + 10;
+        }
+        static int Multiply10(int n)
+        {
+            return n * 10;
+        }
+
+    }
+}
