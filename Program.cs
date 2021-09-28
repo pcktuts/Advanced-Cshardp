931f10b777f10db29f7fe894584603610b7f0da9
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2021-09-29 01:04:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,27 @@
-﻿using System;
-using System.Threading.Tasks;
+using System;
 
-
-namespace MyFirstProject
+namespace myFirstProject
 {
-    class Program
+    class MainClass
     {
-        delegate int ChangeNumber(int n);
+        public delegate int SumDelegate(int x, int y);
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Add10(20));
-            Console.WriteLine(Multiply10(100));
-            ChangeNumber del1 = new ChangeNumber(Add10);
-            Console.WriteLine( del1.Invoke(20));
-
-            ChangeNumber del2 = new ChangeNumber(Multiply10);
-            Console.WriteLine(del2.Invoke(100));
-            Console.WriteLine(del2(100));
-
+            SumDelegate sd = new SumDelegate(Sum);
+            Console.WriteLine(sd(100, 200));
+            SumDelegate sd1 = delegate (int x, int y) { return x + y; };
+            Console.WriteLine(sd1(200, 300));
 
         }
-        static int Add10(int n)
+       
+        static int Sum(int x, int y)
         {
-            return n + 10;
+            return x + y;
         }
-        static int Multiply10(int n)
-        {
-            return n * 10;
-        }
+
+
 
     }
+
 }
