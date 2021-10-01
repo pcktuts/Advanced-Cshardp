5d594d0ac3da58427400f01f43edfa3e5bbd835a
Learn Programming In Malayalam <63596426+pcktuts@users.noreply.github.com>
2021-10-01 21:04:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,42 @@
-﻿using System;
-using System.Threading.Tasks;
+using System;
 
-
-namespace MyFirstProject
+namespace myFirstProject
 {
-    class Program
+    class MainClass
     {
-        delegate int ChangeNumber(int n);
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Add10(20));
-            Console.WriteLine(Multiply10(100));
-            ChangeNumber del1 = new ChangeNumber(Add10);
-            Console.WriteLine( del1.Invoke(20));
+           
+            Console.WriteLine(MainClass.Sum(10, 20));
+            /*
+             * Expression lambda
+             */
+            Func<int, int, int> ExSum = (x, y) => x + y;
+            Console.WriteLine(ExSum(25, 75));
 
-            ChangeNumber del2 = new ChangeNumber(Multiply10);
-            Console.WriteLine(del2.Invoke(100));
-            Console.WriteLine(del2(100));
+            /**
+             * Statement lambda
+             */
+            Func<int, int, int> StSum = (x, y) =>
+            {
+                var sum = x + y;
+                Console.WriteLine($"Sum of {x} and {y} is {sum}");
 
+                return sum;
+            };
+            Console.WriteLine(StSum(34, 78));
+            
 
         }
-        static int Add10(int n)
+
+        static int Sum(int x, int y)
         {
-            return n + 10;
+            return x + y;
         }
-        static int Multiply10(int n)
-        {
-            return n * 10;
-        }
+
+
 
     }
+
 }
